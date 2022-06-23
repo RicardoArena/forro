755f00795df3631585725d3aeecaee82145001ce
Ricardo Arena <arena_717@hotmail.com>
2022-06-23 14:44:17 -0300

    Ricardo component

--- src/components/Ricardo/index.js
+++ src/components/Ricardo/index.js
@@ -1,14 +1,14 @@
 export function Baroes(props) {
   return (
     <>
-      <div class="card" style="width: 18rem;">
+      <div className="card" style="width: 18rem;">
         <img
-          class="card-img-top"
+          className="card-img-top"
           src="https://studiosol-a.akamaihd.net/uploadfile/letras/albuns_high/88984_20200520080028.jpg"
           alt="Card image cap"
         />
-        <div class="card-body">
-          <p class="card-text">
+        <div className="card-body">
+          <p className="card-text">
             Os Barões da Pisadinha é uma banda musical brasileira de forró
             eletrônico e tecnobrega formada por Rodrigo Barão e Felipe Barão em
             dezembro de 2015, nas cidades de Heliópolis e Ribeira do Amparo,
